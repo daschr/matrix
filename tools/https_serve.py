656a9f0e06f96d1b06c4ad217875e1ef4eec0245
David Schramm <david.schramm@live.com>
2020-10-24 16:59:57 +0200

    updated cert

--- tools/https_serve.py
+++ tools/https_serve.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import ssl
+import sys
 
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} [key] [cert]", file=sys.stderr)
+    sys.exit(1)
 
 httpd = HTTPServer(('0.0.0.0', 4443), SimpleHTTPRequestHandler)
 
 httpd.socket = ssl.wrap_socket (httpd.socket, 
-        keyfile="misc/key.pem", 
-        certfile='misc/cert.pem', server_side=True)
+        keyfile=sys.argv[1], 
+        certfile=sys.argv[2], server_side=True)
 
 httpd.serve_forever()
